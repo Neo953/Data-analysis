fc7b5e6a3ac759341cb7abc4868aa135939f9eaa
student <liamnoob0905@gmail.com>
2018-07-11 13:35:13 -0700

    New

--- Main.py
+++ Main.py
@@ -1,2 +1,30 @@
 import pygame
 from pygame .locals import*
+
+pygame.it()
+size = (width, height) = (850,480)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()]
+color = (25,255,255)
+
+bacteria = pygame.sprite.Group()
+doctor = pygame.sprite.Group()
+
+def process_events():
+    global check
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            check = True
+
+
+def main():
+    while True:
+        clock.tick(60)
+        process_events()
+        screen.fill(color)
+        doctor.draw(screen)
+        bacteria.draw(screen)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
